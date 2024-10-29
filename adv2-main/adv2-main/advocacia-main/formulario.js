f6ea4b7a9366e2c7e7bef1d726013ea105cb36e2
rochadudu <128058865+rochadudu@users.noreply.github.com>
2024-10-29 08:48:55 -0300

    Delete adv2-main directory

--- adv2-main/adv2-main/advocacia-main/formulario.js
+++
@@ -1,9 +0,0 @@
-function cadastra_aluno() {
-    const nome = document.getElementById('nome_advogado').value;
-    const dataNascimento = document.getElementById('data_nascimento').value;
-    const oab = document.getElementById('oab').value;
-
-    document.getElementById('mostra_nome').textContent = nome;
-    document.getElementById('mostra_data').textContent = dataNascimento;
-    document.getElementById('mostra_oab').textContent = oab;
-}
